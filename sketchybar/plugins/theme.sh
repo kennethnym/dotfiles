5216ea1f2980c35db93cd6d93c1d1c06d5e31e7a
Kenneth <kennethnym@hey.com>
2025-04-19 14:13:37 +0100

    updated something

--- sketchybar/plugins/theme.sh
+++ sketchybar/plugins/theme.sh
@@ -4,9 +4,9 @@ query=$(defaults read -g AppleInterfaceStyle | xargs)
 
 if [ "$query" = "Dark" ]; then
 	sketchybar --bar color=0xff313244 \
-		         --set '/.*/' icon.color=0xffcdd6f4 label.color=0xffcdd6f4
+		         --set '/.*/' icon.color=0xffffffff label.color=0xffffffff
 else
-	sketchybar --bar color=0xffeff1f5 \
-		         --set '/.*/' icon.color=0xff4c4f69 label.color=0xff4c4f69
+	sketchybar --bar color=0x00eff1f5 \
+		         --set '/.*/' icon.color=0x80000000 label.color=0x80000000
 fi
 
