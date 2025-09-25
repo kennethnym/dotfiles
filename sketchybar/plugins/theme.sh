97c24b2ccd5cf42d257b225a116c7d878505317d
Kenneth <kennethnym@hey.com>
2025-09-25 22:33:44 +0100

    add ghostty themes

--- sketchybar/plugins/theme.sh
+++ sketchybar/plugins/theme.sh
@@ -3,7 +3,7 @@
 query=$(defaults read -g AppleInterfaceStyle | xargs)
 
 if [ "$query" = "Dark" ]; then
-	sketchybar --bar color=0xff313244 \
+	sketchybar --bar color=0x00000000 \
 		         --set '/.*/' icon.color=0xffffffff label.color=0xffffffff
 else
 	sketchybar --bar color=0x00000000 \
